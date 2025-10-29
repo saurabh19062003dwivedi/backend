9fca55708fc51a4b0bf366c5b0b6a8d4a12efa7d
saurabh dwivedi <128312727+saurabh19062003dwivedi@users.noreply.github.com>
2025-10-29 19:47:26 +0530

    Change default PORT from 5000 to 4000

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const fileUpload = require("express-fileupload");
 const dotenv = require("dotenv");
 
 dotenv.config();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 
 //database connect
 database.connect();
